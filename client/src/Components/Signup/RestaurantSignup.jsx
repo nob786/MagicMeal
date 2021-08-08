90933b92ac21a80b450fefbc0e5e5f40b60c63aa
nob786 <nob786@gmail.com>
2021-08-07 20:50:21 -0700

    Front End Updated

--- client/src/Components/Signup/RestaurantSignup.jsx
+++ client/src/Components/Signup/RestaurantSignup.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import axios from "axios";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://magicmeal.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -29,26 +35,25 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
-
   },
   root: {
-    '& .MuiTextField-root': {
+    "& .MuiTextField-root": {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: "25ch",
     },
   },
 }));
@@ -56,23 +61,61 @@ const useStyles = makeStyles((theme) => ({
 export default function RestaurantSignup() {
   const classes = useStyles();
 
-  const [genders, setGender] = React.useState('M');
+  //const [cities, setCity] = React.useState('M');
 
-
-  const gender = [
-    {
-      value: 'Multan',
-      label: 'Multan',
-    },
-    {
-      value: 'Lahore',
-      label: 'Lahore',
-    },
-   
-  ];
+  const [restAdmin, setrestAdmin] = React.useState({
+    OwnerName: "",
+    email: "",
+    password: "",
+    restaurantName: "",
+    restaurantLocation: "",
+    contact: "",
+    category: "",
+    role: "restaurant"
+  });
 
   const handleChange = (event) => {
-    setGender(event.target.value);
+    console.log(event.target.value);
+    console.log(event.target.name);
+    const value = event.target.value;
+    const name = event.target.name;
+
+    setrestAdmin({ ...restAdmin, [name]: value });
+  };
+
+  //=======================================Handle Submmit================================
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //console.log(formData);
+    const {
+      OwnerName,
+      email,
+      password,
+      restaurantName,
+      restaurantLocation,
+      contact, 
+      category,
+      role,
+    } = restAdmin;
+
+    axios
+      .post("http://localhost:3001/auth/signup-restaurant", {
+        ownerName: OwnerName,
+        email: email,
+        password: password,
+        restaurantName: restaurantName,
+        address: restaurantLocation,
+        contact: contact,
+        category: category,
+        role: role,
+      })
+      .then((res) => {
+        console.log(res.data);
+        window.alert("Signup Successfully");
+      })
+      .catch((req) => {
+        window.alert(req.message);
+      });
   };
 
   return (
@@ -82,24 +125,27 @@ export default function RestaurantSignup() {
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
-        <Typography component="h1" variant="h5" style={{color: "black"}}>
+        <Typography component="h1" variant="h5" style={{ color: "black" }}>
           Partener Form
         </Typography>
         <form className={classes.form} noValidate>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
-                autoComplete="fname"
-                name="firstName"
+                autoComplete="name"
+                name="OwnerName"
                 variant="outlined"
                 required
                 fullWidth
-                id="firstName"
-                label="First Name"
+                id="OwnerName"
+                label="Owner Name"
                 autoFocus
+                value={restAdmin.OwnerName}
+                onChange={handleChange}
               />
             </Grid>
-            <Grid item xs={12} sm={6}>
+
+            {/*<Grid item xs={12} sm={6}>
               <TextField
                 variant="outlined"
                 required
@@ -108,33 +154,84 @@ export default function RestaurantSignup() {
                 label="Last Name"
                 name="lastName"
                 autoComplete="lname"
+                value={restAdmin.lastName}
+                onChange={handleChange}
               />
-            
-            </Grid>
+  </Grid>*/}
 
-            <Grid item xs={12} sm={10}>
-                        <TextField
-                    id="outlined-select-currency-native"
-                    select
-                    label="City"
-                    value={gender}
-                    onChange={handleChange}
-                    SelectProps={{
-                        native: true,
-                    }}
-                    helperText="Please select your City"
-                    variant="outlined"
+            {/*     City Drop down
+            <Grid item xs={12}>
+            <FormControl variant="outlined" className={classes.formControl}>
+                  <InputLabel id="demo-simple-select-outlined-label" style={{width: '500px'}}>City</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-outlined-label"
+                      id="demo-simple-select-outlined"
+                      value={cities}
+                      onChange={handleChange}
+                      label="Age"
                     >
-                        {gender.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-                   
-                    </TextField>
-
-
+                  <MenuItem value="">
+                     <em>None</em>
+                  </MenuItem>
+                 <MenuItem value={10}>Multan</MenuItem>
+                <MenuItem value={20}>Lahore</MenuItem>
+                <MenuItem value={30}>Islamabad</MenuItem>
+                <MenuItem value={30}>Karachi</MenuItem>
+                    </Select>
+              </FormControl>
+            </Grid> */}
+                        <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="restaurant_name"
+                label="Restaurant Name"
+                name="restaurantName"
+                value={restAdmin.restaurantName}
+                onChange={handleChange}
+              />
             </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="location"
+                label="Restaurant Location"
+                name="restaurantLocation"
+                value={restAdmin.restaurantLocation}
+                onChange={handleChange}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="mobile"
+                label="Mobile Number"
+                name="contact"
+                value={restAdmin.contact}
+                onChange={handleChange}
+              />
+            </Grid>
+
+            {/*<Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="city"
+                label="City"
+                name="city"
+                autoComplete="city"
+                value={restAdmin.city}
+                onChange={handleChange}
+              />
+          </Grid>*/}
 
             <Grid item xs={12}>
               <TextField
@@ -145,59 +242,64 @@ export default function RestaurantSignup() {
                 label="Email Address"
                 name="email"
                 autoComplete="email"
+                value={restAdmin.email}
+                onChange={handleChange}
               />
             </Grid>
+
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
                 required
                 fullWidth
-                id="restaurant_name"
-                label="Restaurant Name"
-                name="restaurant_name"
+                id="email"
+                label="Password"
+                name="password"
+                autoComplete="password"
+                value={restAdmin.password}
+                onChange={handleChange}
               />
             </Grid>
-           
-              
+
+
+
+
+
             <Grid item xs={12}>
               <TextField
+                id="outlined-textarea"
+                label="Restaurant Category"
+                placeholder="Category"
+                multiline
                 variant="outlined"
-                required
-                fullWidth
-                id="location"
-                label="Restaurant Location"
-                name="description"
+                name="category"
+                value={restAdmin.category}
+                onChange={handleChange}
               />
             </Grid>
-             <Grid item xs={12}>
+
+            <Grid item xs={12}>
             <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="mobile"
-                label="Mobile Number"
-              />
-            </Grid>
+          id="standard-read-only-input"
+          label="Role"
+          defaultValue="Restaurant"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        </Grid>
 
-
-            <Grid item xs={12}>
-                <TextField
-                    id="outlined-textarea"
-                    label="Description about Restaurant"
-                    placeholder="Placeholder"
-                    multiline
-                    variant="outlined"
-                />
-            </Grid>
-            
-            <Grid item xs={12}>
+            {/*<Grid item xs={12}>
               <FormControlLabel
                 control={<Checkbox value="allowExtraEmails" color="primary" />}
                 label="I want to receive inspiration, marketing promotions and updates via email."
               />
-            </Grid>
+        </Grid>*/}
           </Grid>
-          <Button
+
+          <button onClick={handleSubmit}>Submit</button>
+
+          {/*<Button
             type="submit"
             fullWidth
             variant="contained"
@@ -205,7 +307,8 @@ export default function RestaurantSignup() {
             className={classes.submit}
           >
             Apply for Partener Program
-          </Button>
+          </Button>*/}
+
           <Grid container justify="flex-end">
             <Grid item>
               <Link href="#" variant="body2" to="/restaurant-login">
