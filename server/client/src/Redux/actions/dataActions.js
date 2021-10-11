101b4a8fb5266824588585757b1435c5f36d6851
nob786 <nob786@gmail.com>
2021-10-11 14:34:19 +0500

    Updated structure and API's

---
+++ server/client/src/Redux/actions/dataActions.js
@@ -0,0 +1,45 @@
+import { SET_RESTAURANT, SET_RESTAURANTS, PUSH_RESTAURANT_ID } from "./types";
+import axios from "axios";
+
+export const getRestaurants = () => (dispatch) => {
+  axios
+    .get("/user/get-restaurants")
+    .then((res) => {
+      dispatch({
+        type: SET_RESTAURANTS,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      console.log("Error while fetching restaurants");
+      dispatch({
+        type: SET_RESTAURANTS,
+        payload: [],
+      });
+    });
+};
+
+export const getRestaurant = (restId) => (dispatch) => {
+  axios
+    .get(`/user/get-restaurant-menu/${restId}`)
+    .then((res) => {
+      dispatch({
+        type: SET_RESTAURANT,
+        payload: res.data.data.items,
+      });
+    })
+    .catch((err) => {
+      console.log("Error while fetching restaurants");
+      dispatch({
+        type: SET_RESTAURANT,
+        payload: [],
+      });
+    });
+};
+
+export const pushRestaurantId = (restaurantId) => (dispatch) => {
+  dispatch({
+    type: PUSH_RESTAURANT_ID,
+    payload: restaurantId,
+  });
+};
