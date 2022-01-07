e56d4cdaabef4e3f2780ec6b5286735b3bd73ed3
nob786 <nob786@gmail.com>
2022-01-08 00:21:58 +0500

    Last edit

--- server/controllers/publicController.js
+++ server/controllers/publicController.js
@@ -50,24 +50,6 @@ const sendEmail = async (mailOptions) => {
   await emailTransporter.sendMail(mailOptions);
 };
 
-exports.getComments = async (req, res) => {
-  try {
-    const comment = await Comments.find();
-
-    if (!restaurants) return res.status(404).send("No restaurant found.");
-    return res.status(200).json({
-      message: "These are all the restaurants in database.",
-      data: restaurants,
-    });
-  } catch (error) {
-    if (error)
-      return res.status(500).json({
-        message: "Server Error",
-        error: error,
-      });
-  }
-};
-
 exports.subscribeForNewsletter = async (req, res) => {
   console.log("Subscribe Api called");
   console.log("This is my user", process.env.GMAIL_USER);
